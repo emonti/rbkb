0944126dc17d72b7589e715440b0f74938be4717
Eric Monti <emonti@matasano.com>
2009-03-08 18:39:18 -0500

    add cli dir

---
+++ lib/rbkb/cli/hexify.rb
@@ -0,0 +1,70 @@
+#!/usr/bin/env ruby
+
+require 'rbkb/cli'
+
+# The hexify command converts a string or raw data to hex characters. 
+# Input can be # supplied via stdin, a string argument, or a file (with -f).
+class Rbkb::Cli::Hexify < Rbkb::Cli::Executable
+  def make_parser
+    super()
+    add_std_file_arg()
+    arg = @oparse
+
+    #----------------------------------------------------------------------
+    # Add local options
+    arg.banner += " <data | blank for stdin>"
+
+    arg.on("-l", "--length LEN", Numeric, "Output lines of LEN bytes") do |l|
+      bail("Length must be greater than zero") unless (@opts[:len] = l) > 0
+    end
+
+    arg.on("-d", "--delim=DELIMITER", "DELIMITER between each byte") do |d|
+      @opts[:delim] = d
+    end
+
+    arg.on("-p", "--prefix=PREFIX", "PREFIX before each byte") do |p|
+      @opts[:prefix] = p
+    end
+
+    arg.on("-s", "--suffix=SUFFIX", "SUFFIX after each byte") do |s|
+      @opts[:suffix] = s
+    end
+  end
+
+  def parse(*args)
+    super(*args)
+    # blackbag-style spacing compatability
+    if @argv[0] == "+" and @opts[:delim].nil?
+      @opts[:delim]=" "
+      @argv.shift
+    end
+
+    # default string arg
+    if @opts[:indat].nil? and a=@argv.shift
+      @opts[:indat] = a.dup 
+    end
+
+    # catchall
+    bail_args @argv.join(' ') if @argv.length != 0 
+  end
+
+  def go(*args)
+    super(*args)
+
+    # Default to standard input
+    @opts[:indat] ||= @stdin.read() 
+
+    indat = @opts.delete(:indat)
+    len = @opts.delete(:len)
+
+    self.exit(1) unless((len ||= indat.length) > 0)
+
+    until (m = indat.slice!(0..len-1)).empty?
+      @stdout << m.hexify(@opts)
+      @stdout.puts((opts[:delim] and ! indat.empty?)? opts[:delim] : "\n")
+    end
+
+    self.exit(0)
+  end
+end
+
---
+++ lib/rbkb/cli/unhexify.rb
@@ -0,0 +1,53 @@
+#!/usr/bin/env ruby
+
+require 'rbkb/cli'
+
+# unhexify converts a string of hex bytes back to raw data. Input can be 
+# supplied via stdin, a hex-string argument, or a file containing hex (use -f).
+class Rbkb::Cli::Unhexify < Rbkb::Cli::Executable
+  def make_parser
+    super()
+    add_std_file_arg()
+    arg = @oparse
+
+    #----------------------------------------------------------------------
+    # Add local options
+    arg.banner += " <data | blank for stdin>"
+
+    arg.on("-d", "--delim DELIMITER", 
+           "DELIMITER regex between hex chunks") do |d|
+        @opts[:delim] = Regexp.new(d.gsub('\\\\', '\\'))
+    end
+  end
+
+  def parse(*args)
+    super(*args)
+
+    # default string arg
+    if @opts[:indat].nil? and a=@argv.shift
+      @opts[:indat] = a.dup 
+    end
+
+    # catchall
+    bail_args @argv.join(' ') if ARGV.length != 0 
+  end
+
+  def go(*args)
+    super(*args)
+
+    # Default to standard input
+    @opts[:indat] ||= @stdin.read() 
+
+    @opts[:indat].delete!("\r\n")
+    @opts[:delim] ||= /\s*/
+
+    unless out = @opts[:indat].unhexify(@opts[:delim])
+      bail "Error: Failed parsing as hex"
+    end
+
+    @stdout << out
+
+    self.exit(0)
+  end
+end
+
