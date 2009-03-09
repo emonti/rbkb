5434dacd9fcbbdb3db74632912c3b2f3b364901e
Eric Monti <emonti@matasano.com>
2009-03-08 23:22:17 -0500

    refactored dedump

---
+++ lib/rbkb/cli/dedump.rb
@@ -0,0 +1,47 @@
+require 'rbkb/cli'
+
+# Reverses a hexdump back to raw data. Designed to work with hexdumps created 
+# by Unix utilities like 'xxd' as well as 'hexdump -C'.
+class Rbkb::Cli::Dedump < Rbkb::Cli::Executable
+  def initialize(*args)
+    super(*args)
+    @opts[:len] ||= 16
+  end
+
+  def make_parser()
+    arg = super()
+    arg.banner += " <input-file | blank for stdin>"
+
+    arg.on("-l", "--length LEN", Numeric, 
+      "Bytes per line in hexdump (default: #{@opts[:len]})") do |l|
+        bail("Length must be greater than zero") unless (@opts[:len] = l) > 0
+    end
+    return arg
+  end
+
+  def parse(*args)
+    super(*args)
+    parse_file_argument(:indat)
+    parse_catchall()
+  end
+
+  def go(*args)
+    super(*args)
+
+    # Default to standard input
+    @opts[:indat] ||= @stdin.read() 
+
+    self.exit(1) unless((@opts[:len] ||= @opts[:indat].length) > 0)
+
+    begin
+      @opts[:indat].dehexdump( :len => @opts[:len], :out => @stdout)
+    rescue
+      bail "Error: #{$!}"
+    end
+
+    self.exit(0)
+  end
+end
+
+
+
