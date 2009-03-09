f29f8581b7d455bc8b945ecc2c8accb3055a1c5a
Eric Monti <emonti@matasano.com>
2009-03-08 22:26:36 -0500

    refactored crc32

---
+++ lib/rbkb/cli/crc32.rb
@@ -0,0 +1,29 @@
+require 'rbkb/cli'
+
+# crc32 returns a crc32 checksum in hex from stdin or a file
+class Rbkb::Cli::Crc32 < Rbkb::Cli::Executable
+  def initialize(*args)
+    super(*args)
+    @opts[:first] ||= 0
+    @opts[:last]  ||= -1
+  end
+
+  def make_parser()
+    super()
+    add_std_file_opt(:indat)
+    add_range_opts(:first, :last)
+  end
+
+  def parse(*args)
+    super(*args)
+    parse_catchall()
+  end
+
+  def go(*args)
+    super(*args)
+    @opts[:indat] ||= @stdin.read()
+    @stdout.puts @opts[:indat][ @opts[:first] .. @opts[:last] ].crc32.to_hex
+  end
+end
+
+
