8ac92cf1be18e775d0772b2fd45f746f3b3185e9
Eric Monti <emonti@matasano.com>
2009-03-11 20:34:39 -0500

    must have green

--- test/test_rbkb.rb
+++ test/test_rbkb.rb
@@ -8,4 +8,12 @@ class TestRbkb < Test::Unit::TestCase
   def test_truth
     assert true
   end
+
+  # Must... have... green...
+  def test_bones_stuff
+    assert_equal Rbkb::VERSION, Rbkb.version
+    assert_equal File.join(Rbkb::LIBPATH, "blah"), Rbkb.libpath("blah")
+    assert_equal File.join(Rbkb::PATH, "blah"), Rbkb.path("blah")
+  end
+
 end
