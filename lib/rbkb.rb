cafb6cb4f0b251ab54d6259fb1e58033230a9c00
Eric Monti <emonti@matasano.com>
2009-03-16 12:21:50 -0500

    re-enabled recursive require helper

--- lib/rbkb.rb
+++ lib/rbkb.rb
@@ -29,19 +29,18 @@ module Rbkb
     args.empty? ? PATH : ::File.join(PATH, args.flatten)
   end
 
-## TODO decide on keeping/removing this require_all_libs thing
   # Utility method used to require all files ending in .rb that lie in the
   # directory below this file that has the same name as the filename passed
   # in. Optionally, a specific _directory_ name can be passed in such that
   # the _filename_ does not have to be equivalent to the directory.
   #
-#  def self.require_all_libs_relative_to( fname, dir = nil )
-#    dir ||= ::File.basename(fname, '.*')
-#    search_me = ::File.expand_path(
-#        ::File.join(::File.dirname(fname), dir, '**', '*.rb'))
-#
-#    Dir.glob(search_me).sort.each {|rb| require rb}
-#  end
+  def self.require_all_libs_relative_to( fname, dir = nil )
+    dir ||= ::File.basename(fname, '.*')
+    search_me = ::File.expand_path(
+        ::File.join(::File.dirname(fname), dir, '**', '*.rb'))
+
+    Dir.glob(search_me).sort.each {|rb| require rb}
+  end
 
 end  # module Rbkb
 
