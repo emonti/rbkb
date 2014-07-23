a6d35c0fd785bae135034502b1d07ed626aebde5
Eric Monti <em@squareup.com>
2014-07-23 13:51:07 -0700

    add enumerable sum,mean, and standard_deviation

--- lib/rbkb/extends/enumerable.rb
+++ lib/rbkb/extends/enumerable.rb
@@ -6,4 +6,22 @@ module Enumerable
       n.each_recursive(&block) if Enumerable === n
     end
   end
+
+  def sum
+    return self.inject(0){|accum, i| accum + i }
+  end
+
+  def mean
+    return self.sum / self.length.to_f
+  end
+
+  def sample_variance
+    m = self.mean
+    sum = self.inject(0){|accum, i| accum + (i - m) ** 2 }
+    return sum / (self.length - 1).to_f
+  end
+
+  def standard_deviation
+    return Math.sqrt(self.sample_variance)
+  end
 end
--- lib/rbkb/version.rb
+++ lib/rbkb/version.rb
@@ -1,3 +1,3 @@
 module Rbkb
-  VERSION = "0.7.1"
+  VERSION = "0.7.2"
 end
